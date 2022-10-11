991e76f2b2718ffc70910cdd52b6c03309f201ef
tvl2503 <volinhh1804@gmail.com>
2022-10-11 21:06:57 +0700

    fff

---
+++ app.py
@@ -0,0 +1,320 @@
+import chess
+import chess.svg
+import chess.polyglot
+import traceback
+import chess.pgn
+import chess.engine
+from django.shortcuts import redirect
+from flask import Flask, Response, jsonify, render_template, request, url_for
+import webbrowser
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import os
+from flask_session import Session
+from requests import session
+
+
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'volinh01'
+app.config['MYSQL_DB'] = 'chess'
+app.config['UPLOAD_FOLDER']=os.path.join('static','pics')
+
+mysql = MySQL(app)
+
+# Evaluating the board
+pawntable = [
+    0, 0, 0, 0, 0, 0, 0, 0,
+    5, 10, 10, -20, -20, 10, 10, 5,
+    5, -5, -10, 0, 0, -10, -5, 5,
+    0, 0, 0, 20, 20, 0, 0, 0,
+    5, 5, 10, 25, 25, 10, 5, 5,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    0, 0, 0, 0, 0, 0, 0, 0]
+
+knightstable = [
+    -50, -40, -30, -30, -30, -30, -40, -50,
+    -40, -20, 0, 5, 5, 0, -20, -40,
+    -30, 5, 10, 15, 15, 10, 5, -30,
+    -30, 0, 15, 20, 20, 15, 0, -30,
+    -30, 5, 15, 20, 20, 15, 5, -30,
+    -30, 0, 10, 15, 15, 10, 0, -30,
+    -40, -20, 0, 0, 0, 0, -20, -40,
+    -50, -40, -30, -30, -30, -30, -40, -50]
+
+bishopstable = [
+    -20, -10, -10, -10, -10, -10, -10, -20,
+    -10, 5, 0, 0, 0, 0, 5, -10,
+    -10, 10, 10, 10, 10, 10, 10, -10,
+    -10, 0, 10, 10, 10, 10, 0, -10,
+    -10, 5, 5, 10, 10, 5, 5, -10,
+    -10, 0, 5, 10, 10, 5, 0, -10,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -20, -10, -10, -10, -10, -10, -10, -20]
+
+rookstable = [
+    0, 0, 0, 5, 5, 0, 0, 0,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    5, 10, 10, 10, 10, 10, 10, 5,
+    0, 0, 0, 0, 0, 0, 0, 0]
+
+queenstable = [
+    -20, -10, -10, -5, -5, -10, -10, -20,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -10, 5, 5, 5, 5, 5, 0, -10,
+    0, 0, 5, 5, 5, 5, 0, -5,
+    -5, 0, 5, 5, 5, 5, 0, -5,
+    -10, 0, 5, 5, 5, 5, 0, -10,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -20, -10, -10, -5, -5, -10, -10, -20]
+
+kingstable = [
+    20, 30, 10, 0, 0, 10, 30, 20,
+    20, 20, 0, 0, 0, 0, 20, 20,
+    -10, -20, -20, -20, -20, -20, -20, -10,
+    -20, -30, -30, -40, -40, -30, -30, -20,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30]
+
+
+def evaluate_board():
+    if board.is_checkmate():
+        if board.turn:
+            return -9999
+        else:
+            return 9999
+    if board.is_stalemate():
+        return 0
+    if board.is_insufficient_material():
+        return 0
+
+    wp = len(board.pieces(chess.PAWN, chess.WHITE))
+    bp = len(board.pieces(chess.PAWN, chess.BLACK))
+    wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
+    bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
+    wb = len(board.pieces(chess.BISHOP, chess.WHITE))
+    bb = len(board.pieces(chess.BISHOP, chess.BLACK))
+    wr = len(board.pieces(chess.ROOK, chess.WHITE))
+    br = len(board.pieces(chess.ROOK, chess.BLACK))
+    wq = len(board.pieces(chess.QUEEN, chess.WHITE))
+    bq = len(board.pieces(chess.QUEEN, chess.BLACK))
+
+    material = 100 * (wp - bp) + 320 * (wn - bn) + 330 * (wb - bb) + 500 * (wr - br) + 900 * (wq - bq)
+
+    pawnsq = sum([pawntable[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
+    pawnsq = pawnsq + sum([-pawntable[chess.square_mirror(i)]
+                           for i in board.pieces(chess.PAWN, chess.BLACK)])
+    knightsq = sum([knightstable[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
+    knightsq = knightsq + sum([-knightstable[chess.square_mirror(i)]
+                               for i in board.pieces(chess.KNIGHT, chess.BLACK)])
+    bishopsq = sum([bishopstable[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
+    bishopsq = bishopsq + sum([-bishopstable[chess.square_mirror(i)]
+                               for i in board.pieces(chess.BISHOP, chess.BLACK)])
+    rooksq = sum([rookstable[i] for i in board.pieces(chess.ROOK, chess.WHITE)])
+    rooksq = rooksq + sum([-rookstable[chess.square_mirror(i)]
+                           for i in board.pieces(chess.ROOK, chess.BLACK)])
+    queensq = sum([queenstable[i] for i in board.pieces(chess.QUEEN, chess.WHITE)])
+    queensq = queensq + sum([-queenstable[chess.square_mirror(i)]
+                             for i in board.pieces(chess.QUEEN, chess.BLACK)])
+    kingsq = sum([kingstable[i] for i in board.pieces(chess.KING, chess.WHITE)])
+    kingsq = kingsq + sum([-kingstable[chess.square_mirror(i)]
+                           for i in board.pieces(chess.KING, chess.BLACK)])
+
+    eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
+    if board.turn:
+        return eval
+    else:
+        return -eval
+
+
+# Searching the best move using minimax and alphabeta algorithm with negamax implementation
+def alphabeta(alpha, beta, depthleft):
+    bestscore = -9999
+    if (depthleft == 0):
+        return quiesce(alpha, beta)
+    for move in board.legal_moves:
+        board.push(move)
+        score = -alphabeta(-beta, -alpha, depthleft - 1)
+        board.pop()
+        if (score >= beta):
+            return score
+        if (score > bestscore):
+            bestscore = score
+        if (score > alpha):
+            alpha = score
+    return bestscore
+
+
+def quiesce(alpha, beta):
+    stand_pat = evaluate_board()
+    if (stand_pat >= beta):
+        return beta
+    if (alpha < stand_pat):
+        alpha = stand_pat
+
+    for move in board.legal_moves:
+        if board.is_capture(move):
+            board.push(move)
+            score = -quiesce(-beta, -alpha)
+            board.pop()
+
+            if (score >= beta):
+                return beta
+            if (score > alpha):
+                alpha = score
+    return alpha
+
+
+def selectmove(depth):
+    try:
+        # move = chess.polyglot.MemoryMappedReader("C:/Users/Linh Tran Vo/Downloads/Chess-World-master/books/human.bin").weighted_choice(board).move
+        move = chess.polyglot.MemoryMappedReader("C:/Users/Linh Tran Vo/Downloads/books/computer.bin").weighted_choice(board).move
+        # move = chess.polyglot.MemoryMappedReader("C:/Users/Linh Tran Vo/Downloads/books/pecg_book.bin").weighted_choice(board).move
+        return move
+    except:
+        bestMove = chess.Move.null()
+        bestValue = -99999
+        alpha = -100000
+        beta = 100000
+        for move in board.legal_moves:
+            board.push(move)
+            boardValue = -alphabeta(-beta, -alpha, depth - 1)
+            if boardValue > bestValue:
+                bestValue = boardValue
+                bestMove = move
+            if (boardValue > alpha):
+                alpha = boardValue
+            board.pop()
+        return bestMove
+
+def devmove():
+    move = selectmove(3)
+    
+    board.push(move)
+
+
+# Searching Stockfish's Move
+def stockfish():
+    engine = chess.engine.SimpleEngine.popen_uci(
+        "C:/Users/Linh Tran Vo/Downloads/Chess-World-master/engines/stockfish.exe")
+    move = engine.play(board, chess.engine.Limit(time=0.1))
+    
+    board.push(move.move)
+
+
+
+
+
+
+
+# Front Page of the Flask Web Page
+@app.route("/")
+def main():
+
+    if not session.get("user"):
+        return redirect("/login")
+  
+    global count, board
+    if count == 1:
+        count += 1
+    return render_template("index.html", src = "/board.svg")
+    
+
+# Display Board
+@app.route("/board.svg/")
+def board():
+    return Response(chess.svg.board(board=board, size=700), mimetype='image/svg+xml')
+
+@app.route("/login")
+def login():
+    if request.method == 'POST' : 
+        _tk = request.form['tk']
+        _mk = request.form['mk']
+        acc= check_acc(_tk, _mk)
+
+        if not acc:
+            return render_template('login.html', error='Sai thông tin đăng nhập!')
+        else:
+            session["user"] = request.form.get("_tk")
+            redirect("/")
+    return render_template('login.html')
+@app.route("/btnlogin", methods=['POST'])
+def btnlogin():
+    _tk = request.form['tk']
+    _mk = request.form['mk']
+    print(_tk, _mk)
+    acc= check_acc(_tk, _mk)
+    if not acc:
+        return render_template('login.html', error='Sai thông tin đăng nhập!')
+    else:
+        session["user"] = _tk
+        return redirect(url_for("/", src = "./board.svg"))
+def check_acc(x,y):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM player WHERE TK = %s AND MK = %s', (x, y,))
+    account = cursor.fetchone()
+    if not account:
+        return None
+    else:
+        return account
+
+def add_acc(x,y,z):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('INSERT INTO player ( TK, MK, Ten, Diem) VALUES (%s,%s,%s,0)', (x,y,z))
+    mysql.connection.commit()
+
+@app.route("/move/")
+def move():
+    try:
+        move = request.args.get('move', default="")
+        
+        board.push_san(move)
+    except Exception:
+        traceback.print_exc()
+    return main()
+
+# Make UCI Compatible engine's move
+@app.route("/engine/", methods=['POST'])
+def engine():
+    try:
+        stockfish()
+        # cdrill()
+        # deuterium()
+    except Exception:
+        traceback.print_exc()
+    return main()
+
+
+# New Game
+@app.route("/game/", methods=['POST'])
+def game():
+    board.reset()
+    return main()
+
+
+# Undo
+@app.route("/undo/", methods=['POST'])
+def undo():
+    try:
+        board.pop()
+    except Exception:
+        traceback.print_exc()
+    return main()
+
+
+
+# Main Function
+if __name__ == '__main__':
+    count = 1
+    board = chess.Board()
+    webbrowser.open("http://127.0.0.1:5000/")
+    app.run()
